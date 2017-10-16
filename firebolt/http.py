a6c57ff5aca6e2892f6b81baa1881231f74ee650
Sam Ireland <sam.ireland.uk@gmail.com>
2017-10-16 23:22:02 +0100

    Tabs to spaces

--- firebolt/http.py
+++ firebolt/http.py
@@ -1,63 +1,63 @@
 """The HTTP module - contains Request and Response functionality."""
 
 class Request:
-	"""A HTTP Request.
+    """A HTTP Request.
 
-	:param str uri: The URI of the request.
-	:param str method: The HTTP method of the request. Will be converted to\
-	upper case.
-	:param dict headers: A ``dict`` of HTTP header field name and values.
-	:raises TypeError: if the URI is not a string.
-	:raises TypeError: if the method is not a string."""
+    :param str uri: The URI of the request.
+    :param str method: The HTTP method of the request. Will be converted to\
+    upper case.
+    :param dict headers: A ``dict`` of HTTP header field name and values.
+    :raises TypeError: if the URI is not a string.
+    :raises TypeError: if the method is not a string."""
 
-	def __init__(self, uri, method="GET", headers=None):
-		if not isinstance(uri, str):
-			raise TypeError(("uri {} is not str".format(uri)))
-		self._uri = uri
-		if not isinstance(method, str):
-			raise TypeError(("method {} is not str".format(method)))
-		self._method = method.upper()
-		self._headers = {} if headers is None else headers
-
-
-	@property
-	def uri(self):
-		"""The Uniform Resource Identifier - the string that points to the
-		resource that the request is asking for. It must be a string.
-
-		:rtype: ``str``"""
-
-		return self._uri
+    def __init__(self, uri, method="GET", headers=None):
+        if not isinstance(uri, str):
+            raise TypeError(("uri {} is not str".format(uri)))
+        self._uri = uri
+        if not isinstance(method, str):
+            raise TypeError(("method {} is not str".format(method)))
+        self._method = method.upper()
+        self._headers = {} if headers is None else headers
 
 
-	@uri.setter
-	def uri(self, uri):
-		if not isinstance(uri, str):
-			raise TypeError(("uri {} is not str".format(uri)))
-		self._uri = uri
+    @property
+    def uri(self):
+        """The Uniform Resource Identifier - the string that points to the
+        resource that the request is asking for. It must be a string.
+
+        :rtype: ``str``"""
+
+        return self._uri
 
 
-	@property
-	def method(self):
-		"""The HTTP method of the request. It must be a string, and will be\
-		converted to upper case.
-
-		:rtype: ``str``"""
-
-		return self._method
+    @uri.setter
+    def uri(self, uri):
+        if not isinstance(uri, str):
+            raise TypeError(("uri {} is not str".format(uri)))
+        self._uri = uri
 
 
-	@method.setter
-	def method(self, method):
-		if not isinstance(method, str):
-			raise TypeError(("method {} is not str".format(method)))
-		self._method = method.upper()
+    @property
+    def method(self):
+        """The HTTP method of the request. It must be a string, and will be\
+        converted to upper case.
+
+        :rtype: ``str``"""
+
+        return self._method
 
 
-	@property
-	def headers(self):
-		"""The HTTP headers of the request, as a ``dict``
+    @method.setter
+    def method(self, method):
+        if not isinstance(method, str):
+            raise TypeError(("method {} is not str".format(method)))
+        self._method = method.upper()
 
-		:rtype: ``dict``"""
-		
-		return dict(self._headers)
+
+    @property
+    def headers(self):
+        """The HTTP headers of the request, as a ``dict``
+
+        :rtype: ``dict``"""
+        
+        return dict(self._headers)
